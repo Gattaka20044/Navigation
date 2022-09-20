dd446c0d941267ff6bd4d65870be6ffab335cad5
Gattaka20044 <nexti20044@mail.ru>
2022-09-20 13:42:42 +0300

    Урок 1. Jetpack Navigation Component. -перемещение между фрагментами -передача информации на предыдущий фрагмент через Fragment Result API

---
+++ app/src/main/java/com/bignerdranch/android/navigation/SecretFragment.kt
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.navigation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bignerdranch.android.navigation.databinding.FragmentSecretBinding
+
+class SecretFragment : Fragment(R.layout.fragment_secret) {
+
+    private lateinit var binding: FragmentSecretBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSecretBinding.bind(view)
+
+        binding.closeBoxButton.setOnClickListener {
+            findNavController().popBackStack(R.id.rootFragment, false)
+        }
+        binding.goBackButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+}
